5d1e2731f9816d844e17cbed4ba662af62434a0b
EarnPlayApps <earnplayapps@gmail.com>
2026-09-20 03:58:11 +0800

    Reduce APK size by using Play Services barcode model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("androidx.camera:camera-lifecycle:1.4.1")
     implementation("androidx.camera:camera-view:1.4.1")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+    implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.1")
     implementation("com.google.android.gms:play-services-ads:24.7.0")
     implementation("com.google.guava:guava:33.3.1-android")
 }
