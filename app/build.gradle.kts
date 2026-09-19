b6d2bd7a4d2087d90cc075f225c209e458e6f41e
EarnPlayApps <earnplayapps@gmail.com>
2026-09-19 22:47:40 +0800

    Add Google Mobile Ads SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,4 +36,5 @@ dependencies {
     implementation("androidx.camera:camera-view:1.4.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.mlkit:barcode-scanning:17.3.0")
+    implementation("com.google.android.gms:play-services-ads:25.4.0")
 }
